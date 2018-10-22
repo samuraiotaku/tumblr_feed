9224061fd124f0020210e461cdfa209d4cd82693
samuraiotaku <bshimizu@csumb.edu>
2018-10-22 01:07:43 -0700

    finish lab1

--- tumblr_feed/PhotoCell.swift
+++ tumblr_feed/PhotoCell.swift
@@ -3,30 +3,21 @@
 //  tumblr_feed
 //
 //  Created by Brandon Shimizu on 9/18/18.
-//  Copyright © 2018 Brandon Shimizu. All rights reserved.
+//  Copyright © 2018 Brandon Shimizu. All rights reserved. test
 //
 
 import UIKit
 
 class PhotoCell: UITableViewCell {
 
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
-        
-        // Configure YourCustomCell using the outlets that you've defined.
-        
-        return cell
-    }
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- tumblr_feed/View Controller/PhotosViewController.swift
+++ tumblr_feed/View Controller/PhotosViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var posts: [[String: Any]] = []
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.delegate = self
         tableView.dataSource = self
-        
+        //self.tableView.rowHeight = 44.0
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -33,9 +35,9 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 // Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
             }
+        self.tableView.reloadData()
         }
         task.resume()
-
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,17 +45,22 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-        cell.textLabel?.text = "This is row \(indexPath.row)"
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        //cell.textLabel?.text = "This is row \(indexPath.row)"
+        let post = posts[indexPath.row]
+        if let photos = post["photos"] as? [[String: Any]] {
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            cell.photoImageView.af_setImage(withURL: url!)
+        }
         return cell
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-    
-    
 
 }
